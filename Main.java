29bdfb9671ddd5092bdc46e6ca5e05933f934646
Leonid Kazarin <124534184+lkazarin@users.noreply.github.com>
2023-03-21 13:20:46 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        byte a = 25;
+        short b = 1575;
+        int c = 1000000;
+        long d = 10000000000l;
+        float e = 25.35f;
+        double f = 2.375;
+        char g = 'd';
+        boolean h = true;
+        System.out.println(a + " " + b + " " + c + " " + d + " " + e + " " + d + " " + f + " " + g + " " + h);
+    }
+}
